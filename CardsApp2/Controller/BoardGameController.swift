a22acad8b1f68a1351e16f1d66f5d0d8f7633bcb
DMYTRO <dmytro.neboha@proton.me>
2022-12-22 19:45:26 +0200

    20.2 Разработка модели

--- CardsApp2/Controller/BoardGameController.swift
+++ CardsApp2/Controller/BoardGameController.swift
@@ -9,6 +9,17 @@ class BoardGameController: UIViewController {
         super.viewDidLoad()
 
     }
-
+    
+    // количество уникальных карточек
+    var cardsPairsCount = 8
+    // сущность "Игра"
+    lazy var game: Game = getNewGame()
+    
+    private func getNewGame() -> Game {
+        let game = Game()
+        game.cardsCount = self.cardsPairsCount
+        game.generateCards()
+        return game
+    }
 
 }
